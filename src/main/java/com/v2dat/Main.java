0c31ed7639bf8bba1173da1ec5cd72bcaa744c3e
liujiawei <zj.liujiawei@aisino.com>
2024-08-09 09:03:34 +0800

    add cli support

--- src/main/java/com/v2dat/Main.java
+++ src/main/java/com/v2dat/Main.java
@@ -3,7 +3,10 @@ package com.v2dat;
 import com.v2dat.unpack.GeoIPUnPacker;
 import com.v2dat.unpack.GeoSiteUnPacker;
 import com.v2dat.unpack.UnPacker;
+import org.apache.commons.cli.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,12 +16,59 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // GeoIPUnPacker test case
-        UnPacker geoIPUnPacker = new GeoIPUnPacker();
-        geoIPUnPacker.unpack("/Users/liujiawei/Documents/github/gfw2dnsmasq/ip/geoip.dat", "/Users/liujiawei/Desktop/geo/geoip", List.of("cn"), true);
+        Options options = new Options();
 
-        // GeoSiteUnPacker test case
-        UnPacker geoSiteUnPacker = new GeoSiteUnPacker();
-        geoSiteUnPacker.unpack("/Users/liujiawei/Documents/github/gfw2dnsmasq/domain/geosite.dat", "/Users/liujiawei/Desktop/geo/geosite", List.of("gfw"), true);
+        Option unpack = new Option("u", "unpack", true, "unpack type");
+        unpack.setRequired(true);
+        options.addOption(unpack);
+
+        Option outDir = new Option("d", "dir", true, "out dir");
+        outDir.setRequired(true);
+        options.addOption(outDir);
+
+        Option tag = new Option("t", "tag", true, "filter tags");
+        tag.setRequired(false);
+        options.addOption(tag);
+
+        Option file = new Option("f", "file", true, "data file");
+        file.setRequired(true);
+        options.addOption(file);
+
+        Option clean = new Option("c", "clean", true, "clean out dir if need");
+        clean.setRequired(false);
+        options.addOption(clean);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+        try {
+            cmd = parser.parse(options, args);
+            String unpackTypeVal = cmd.getOptionValue("unpack");
+            String outDirVal = cmd.getOptionValue("dir");
+            String fileVal = cmd.getOptionValue("file");
+            String tagVal = cmd.getOptionValue("tag");
+            String cleanVal = cmd.getOptionValue("clean");
+            List<String> tags = new ArrayList<>();
+            if (tagVal != null && !tagVal.isEmpty()) {
+                tags.addAll(Arrays.asList(tagVal.split(",")));
+            }
+            boolean cleanFlag = !"false".equals(cleanVal);
+
+            switch (unpackTypeVal) {
+                case "geoip" -> {
+                    UnPacker geoIPUnPacker = new GeoIPUnPacker();
+                    geoIPUnPacker.unpack(fileVal, outDirVal, tags, cleanFlag);
+                }
+                case "geosite" -> {
+                    UnPacker geoSiteUnPacker = new GeoSiteUnPacker();
+                    geoSiteUnPacker.unpack(fileVal, outDirVal, tags, cleanFlag);
+                }
+                default -> {
+                }
+            }
+        } catch (ParseException e) {
+            formatter.printHelp("utility-name", options);
+            System.exit(1);
+        }
     }
 }
